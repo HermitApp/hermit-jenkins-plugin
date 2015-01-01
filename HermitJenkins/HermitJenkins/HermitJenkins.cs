b15f6965c46583cd56ae5accb75cbb7c6b270cce
Lucas Brendel <lucasbrendel@gmail.com>
2014-12-31 22:31:49 -0600

    add initial project files

---
+++ HermitJenkins/HermitJenkins/HermitJenkins.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hermit.Plugin;
+using System.Reflection;
+
+namespace HermitJenkins
+{
+    public class HermitJenkins : IPlugin
+    {
+        private HermitJenkinsSettingsViewModel _Settings;
+
+        public string Author
+        {
+            get { return "Lucas Brendel"; }
+        }
+
+        public string Description
+        {
+            get { return "Jenkins job monitor"; }
+        }
+
+        public string Name
+        {
+            get { return "Jenkins"; }
+        }
+
+        public void SaveSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISettingsItem Settings
+        {
+            get { return _Settings; }
+        }
+
+        public string Version
+        {
+            get { return Assembly.GetEntryAssembly().GetName().Version.ToString(); }
+        }
+
+        public HermitJenkins()
+        {
+            _Settings = new HermitJenkinsSettingsViewModel();
+        }
+    }
+}
---
+++ HermitJenkins/HermitJenkins/HermitJenkinsViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hermit.Plugin;
+using Caliburn.Micro;
+
+namespace HermitJenkins
+{
+    public class HermitJenkinsViewModel : Screen, IShell
+    {
+        public string DisplayName
+        {
+            get
+            {
+                return "Jenkins";
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private JenkinsNode _Node;
+        public JenkinsNode Node
+        {
+            get { return _Node; }
+            set
+            {
+                if (value != _Node)
+                {
+                    _Node = value;
+                    NotifyOfPropertyChange(() => Node);
+                }
+            }
+        }
+    }
+
+    public class JenkinsNode : PropertyChangedBase
+    {
+        private string _Description;
+        public string Description
+        {
+            get { return _Description; }
+            set
+            {
+                if (value != _Description)
+                {
+                    _Description = value;
+                    NotifyOfPropertyChange(() => Description);
+                }
+            }
+        }
+
+        private List<JenkinsProject> _Projects;
+        public List<JenkinsProject> Projects
+        {
+            get { return _Projects; }
+            set
+            {
+                if (value != _Projects)
+                {
+                    _Projects = value;
+                    NotifyOfPropertyChange(() => Projects);
+                }
+            }
+        }
+    }
+
+    public class JenkinsProject : PropertyChangedBase
+    {
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (value != _Name)
+                {
+                    _Name = value;
+                    NotifyOfPropertyChange(() => Name);
+                }
+            }
+        }
+
+        private string _ImageStatus;
+        public string ImageStatus
+        {
+            get { return _ImageStatus; }
+            set
+            {
+                if (value != _ImageStatus)
+                {
+                    if (value != _ImageStatus)
+                    {
+                        _ImageStatus = value;
+                        NotifyOfPropertyChange(() => ImageStatus);
+                    }
+                }
+            }
+        }
+
+        public JenkinsProject()
+        {
+
+        }
+
+        public void CheckProjectStatus()
+        {
+
+        }
+    }
+}
